b98a6e0af5c7260c5967f584ef2012bd735d8004
DiaryKyosuke <fendianwar36@gmail.com>
2020-12-07 16:44:46 +0700

    update

--- src/helpers/email.js
+++ src/helpers/email.js
@@ -1,7 +1,7 @@
 const nodemailer = require("nodemailer")
 // create reusable transporter object using the default SMTP transport
 let transporter = nodemailer.createTransport({
-  host: "imap.gmail.com",
+  host: "smtp.gmail.com",
   port: 465,
   secure: true,
   service: 'gmail',
